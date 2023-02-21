e25edcc23e3b9e57ccd70b510013e01005a87e83
nandankumarms <nandankumarms31@gmail.com>
2023-02-21 13:24:03 +0530

    initial commit

---
+++ src/main/java/com/nandan/vdms/VdmsApplication.java
@@ -0,0 +1,13 @@
+package com.nandan.vdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdmsApplication.class, args);
+	}
+
+}
